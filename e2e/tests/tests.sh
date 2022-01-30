afcaae0153fa13b3b340d2fb17e4ea43fe2feab4
fredflev <frederic.leveque@r3.com>
2022-01-30 22:15:32 +0800

    uncomment

--- e2e/tests/tests.sh
+++ e2e/tests/tests.sh
@@ -24,20 +24,20 @@ sleep 3000000
 
 new=$(phantomjs render.js "http://result:4000/" | grep -i vote | cut -d ">" -f 4 | cut -d " " -f1)
 
-# echo -e "\n\n-----------------"
-# echo -e "New Votes Count: $new"
-# echo -e "-----------------\n"
+echo -e "\n\n-----------------"
+echo -e "New Votes Count: $new"
+echo -e "-----------------\n"
 
-# echo -e "I: Checking if votes tally......\n"
+echo -e "I: Checking if votes tally......\n"
 
-# if [ "$next" -eq "$new" ]; then
-#   echo -e "\\e[42m------------"
-#   echo -e "\\e[92mTests passed"
-#   echo -e "\\e[42m------------"
-#   exit 0
-# else
-#   echo -e "\\e[41m------------"
-#   echo -e "\\e[91mTests failed"
-#   echo -e "\\e[41m------------"
-#   exit 1
-# fi
+if [ "$next" -eq "$new" ]; then
+  echo -e "\\e[42m------------"
+  echo -e "\\e[92mTests passed"
+  echo -e "\\e[42m------------"
+  exit 0
+else
+  echo -e "\\e[41m------------"
+  echo -e "\\e[91mTests failed"
+  echo -e "\\e[41m------------"
+  exit 1
+fi
