043896d028d540f9e22fa065d69d77910c8acd68
fredflev <frederic.leveque@r3.com>
2022-02-03 16:57:13 +0800

    update integration test script

--- vote/integration_test.sh
+++ vote/integration_test.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-cd integration
+set -o nounset
+set -o pipefail
+
+cd integration || exit
 
 echo "I: Creating environment to run  integration tests..."
 
 docker-compose build
 docker-compose up -d
 
-
 echo "I: Launching Integration Test ..."
 
 docker-compose run --rm integration /test/test.sh
 
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
   echo "---------------------------------------"
   echo "INTEGRATION TESTS PASSED....."
   echo "---------------------------------------"
